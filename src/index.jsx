0139e7e71ec85ac3e0901b780e2762ee43e6d7d9
Hanh NG <moitmp64@gmail.com>
2024-03-28 16:19:01 +0100

    Ajouter le router

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './pages/Home';
 import './utils/style/index.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
